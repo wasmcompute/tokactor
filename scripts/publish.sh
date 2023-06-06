28f66a63360d40d26d67d0473291ca32b0451fb2
Alec Di Vito <divito.alec@gmail.com>
2023-06-05 20:40:19 -0400

    release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "Publishing"
-
 version=$1
 token=$CARGO_REGISTRY_TOKEN
 
 toml set Cargo.toml package.version $version > output.toml 2>&1 
 mv output.toml Cargo.toml
-cargo publish --dry-run --allow-dirty --token $token
+cargo publish --allow-dirty --token $token
--- scripts/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version="$1"
-token=$CARGO_REGISTRY_TOKEN
-
-toml set Cargo.toml package.version $version > output.toml 2>&1 
-mv output.toml Cargo.toml
-cargo publish --dry-run --allow-dirty --token $token
